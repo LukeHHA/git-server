8be4c31f60f9454ecf36452f06cf58906bcd44dc
Luke Howe <lukeh@Lukes-Mac-mini.local>
2025-12-01 16:26:02 +0800

    added endpoint for testing create repo func

--- gitserve/serve.py
+++ gitserve/serve.py
@@ -1,11 +1,14 @@
 from flask import Blueprint
+from src import actions
 
 # from werkzeug.security import check_password_hash, generate_password_hash
 
 bp: Blueprint = Blueprint("serve", __name__, url_prefix="/serve")
 
-bp.route("/init", methods=("GET", "POST"))
+bp.route("/init_repo", methods=("GET", "POST"))
+
+act: actions.Actions = actions.Actions()
 
 
-def init() -> None:
-    return
+def init_repo() -> None:
+    act.create_repo("helloWorld")
